0c0a5bb78a2f8ee55d0f775b1cced2a839218255
Konstantin Werner <konstantin.werner@gmail.com>
2023-01-29 01:15:44 +0100

    Initial Commit

---
+++ js/cmd.js
@@ -0,0 +1,278 @@
+const REMregister = [
+    { use: true,  name: "Configuration", display: "checkbox" },
+    { use: false },
+    { use: false },
+    { use: false },
+    { use: false },
+    { use: false },
+    { use: false },
+    { use: false },
+    
+    { use: true,  name: "Offset X", display: "unsigned", min: 0, max:0xFFFF, unit: "stp" },
+    { use: true,  name: "Offset Y", display: "unsigned", min: 0, max:0xFFFF, unit: "stp" },
+    { use: true,  name: "Steps X",  display: "unsigned", min: 0, max:0xFFFF},
+    { use: true,  name: "Steps Y",  display: "unsigned", min: 0, max:0xFFFF},
+    { use: true,  name: "Delta X",  display: "unsigned", min: 0, max:0xFFFF, unit: "stp" },
+    { use: true,  name: "Delta Y",  display: "unsigned", min: 0, max:0xFFFF, unit: "stp" },
+    
+    { use: false },
+    { use: false },
+
+    { use: true,  name: "Ctrl. Delay", display: "unsigned", min: 0, max:0xFFFF, unit: "ms", mult: 0.00256 },
+    { use: true,  name: "Init Delay",  display: "unsigned", min: 0, max:0xFFFF, unit: "us", mult: 0.01 },
+    { use: true,  name: "Col. Delay",  display: "unsigned", min: 0, max:0xFFFF, unit: "us", mult: 0.01  },
+    { use: true,  name: "Row Delay",   display: "unsigned", min: 0, max:0xFFFF, unit: "us", mult: 0.01  },
+
+    { use: true,  name: "Transform 00", display: "signed", min: -2^15, max: 2^15-1, div: 0x4000 },
+    { use: true,  name: "Transform 01", display: "signed", min: -2^15, max: 2^15-1, div: 0x4000 },
+    { use: true,  name: "Transform 02", display: "signed", min: -2^15, max: 2^15-1, div: 0x4000 },
+    { use: true,  name: "Transform 10", display: "signed", min: -2^15, max: 2^15-1, div: 0x4000 },
+    { use: true,  name: "Transform 11", display: "signed", min: -2^15, max: 2^15-1, div: 0x4000 },
+    { use: true,  name: "Transform 12", display: "signed", min: -2^15, max: 2^15-1, div: 0x4000 },
+
+    { use: false },
+    { use: false },
+    { use: false },
+
+    { use: true,  name: "Clock Freq",  display: "unsigned", readonly: true },
+    { use: true,  name: "PCB Version", display: "hex", readonly: true },
+    { use: true,  name: "FW Version",  display: "hex", readonly: true },
+    { use: true,  name: "FW Build",    display: "hex", readonly: true }
+];
+
+const REMevents = Object.freeze({
+    CONNECTION_OPENED: Symbol("Connection opened"),
+    CONNECTION_CLOSED: Symbol("Connection closed"),
+    IMAGE_RECEIVED: Symbol("New Image received"),
+    REGISTER_CHANGE: Symbol("Register Value changed")
+})
+
+class REMinterface {
+    constructor()
+    {
+        this.events = new Map();
+
+        this.knownEvents = new Set(
+            [REMevents.CONNECTION_OPENED,
+            REMevents.CONNECTION_CLOSED,
+            REMevents.IMAGE_RECEIVED,
+            REMevents.REGISTER_CHANGE]);
+  
+        this.serial = new Serial();
+
+        this.serial.on(SerialEvents.CONNECTION_OPENED, this.onSerialOpened.bind(this));
+        this.serial.on(SerialEvents.CONNECTION_CLOSED, this.onSerialClosed.bind(this));
+        this.serial.on(SerialEvents.DATA_RECEIVED, this.onSerialReceived.bind(this));
+        this.serial.on(SerialEvents.ERROR_OCCURRED, this.onSerialError.bind(this));
+
+        this.mode = "cmd";
+        this.textDecoder = new TextDecoder();
+        this.rx_buffer = "";
+        this.response_buffer = [];
+
+        this.register = [];
+
+        this.image = [];
+        this.count = 0;
+    }
+
+    on(label, callback) {
+        if (this.knownEvents.has(label)) {
+            if (!this.events.has(label)) {
+                this.events.set(label, []);
+            }
+            this.events.get(label).push(callback);
+        } else {
+            console.log(`Could not create event subscription for ${label}. Event unknown.`);
+        }
+    }
+
+    fireEvent(event, data = null) {
+        if (this.events.has(event)) {
+            for (let callback of this.events.get(event)) {
+                callback(this, data);
+            }
+        }
+    }
+
+    async connect() {
+        const serialOptions = {
+            baudRate: 921600,
+            dataBits: 8,
+            stopBits: 1,
+            parity: "none",
+            flowControl: "none",
+            bufferSize: 1024
+        };
+        
+        if (navigator.serial) {
+            await this.serial.autoConnectAndOpenPreviouslyApprovedPort(serialOptions);
+
+/*            if (!this.serial.isOpen()) {
+                await this.serial.connectAndOpen(
+                    [{ usbVendorId: 0x0403, usbProductId: 0x6011 }],
+                    serialOptions);
+            }
+*/           
+            
+        } else {
+            alert('The Web Serial API is not supported on this browser.');
+        }
+    }
+
+    async close() {
+        await this.serial.close();
+    }
+
+    async command(cmd, wait=true) {
+        console.log("Sending Command " + cmd);
+        await this.serial.write(cmd);
+
+        while (wait)
+        {
+            await sleep(2);
+            if (this.response_buffer.length > 0)
+            {
+                const response = this.response_buffer.pop();
+                return response;
+            }
+        }
+    }
+
+    async readRegister(nr)
+    {
+        if (!this.serial.isOpen()) 
+            return false;
+
+        const regex = /\{R([a-fA-F0-9]{4})\}/;
+
+        var response = await this.command("{R" + dec2hex(nr, 2) + "}");
+        
+        var regval = regex.exec(response);
+
+        if (regval && regval.length == 2)
+        {
+            var val = parseInt(regval[1], 16);
+
+            if (this.register[nr] != val)
+                this.fireEvent(REMevents.REGISTER_CHANGE, {nr: nr, val: val});
+
+            this.register[nr] = val;
+            return this.register[nr];
+        } else
+            return undefined;        
+    }
+
+    async writeRegister(nr, val)
+    {
+        if (!this.serial.isOpen()) 
+            return false;
+
+        var response = await this.command("{W" + dec2hex(nr, 2) + dec2hex(val, 4) + "}");
+
+        if (response.match("!"))
+        {
+            if (this.register[nr] != val)
+                this.fireEvent(REMevents.REGISTER_CHANGE, {nr: nr, val: val});
+
+            this.register[nr] = val;
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    async scan()
+    {
+        if (!this.serial.isOpen()) 
+            return false;
+
+        await this.readRegister(10);
+        await this.readRegister(11);
+
+        this.count = 0;
+        this.image = [];
+
+        this.mode = "data";
+        this.command("{S}", false);
+    }
+
+    async abort()
+    {
+        if (!this.serial.isOpen()) 
+            return false;
+
+        await this.command("{X}");
+        this.mode = "cmd";
+    }
+
+    async init()
+    {
+        if (!this.serial.isOpen()) 
+            return false;
+
+        for (var r = 0; r < 32; ++r)
+        {
+            this.register[r] = await this.readRegister(r);
+        }
+    }
+
+    onSerialError(eventSender, error) {
+        console.log("onSerialErrorOccurred", error);
+    }
+
+    onSerialOpened(eventSender) {
+        this.fireEvent(REMevents.CONNECTION_OPENED, eventSender);
+    }
+
+    onSerialClosed(eventSender) {
+        this.fireEvent(REMevents.CONNECTION_CLOSED, eventSender);
+    }
+
+    onSerialReceived(eventSender, newData) {
+        if (this.mode == "cmd")
+        {
+            this.rx_buffer += this.textDecoder.decode(newData);
+            const responses = this.rx_buffer.split("\n");
+            this.rx_buffer = responses.pop();
+            this.response_buffer = this.response_buffer.concat(responses);
+        } else {
+            var tmp = new Uint8Array(this.image.length + newData.length);
+            tmp.set(this.image);
+            tmp.set(newData, this.image.length);
+            this.image = tmp;
+            this.count += newData.length;
+            
+            if (this.count >= this.register[10] * this.register[11])
+            {
+                this.mode = "cmd";
+                this.onImageReceived();
+            }
+        }
+    }
+
+    onImageReceived()
+    {
+        this.fireEvent(REMevents.IMAGE_RECEIVED, {
+            h: this.register[11],
+            w: this.register[10],
+            image: this.image
+        });
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function dec2hex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
---
+++ js/serial.js
@@ -0,0 +1,280 @@
+// Class to handle serial communications. Currently, only supports text input/output rather than binary data
+// Based on https://web.dev/serial/
+//
+// Currently, this code only works on Chrome and *only* if you enable an experimental flag:
+// 1. First, type chrome://flags in the address bar
+// 2. Then, in the search box, type "experimental-web-platform-features"
+// 3. This flag should be set to "Enabled"
+// 4. Restart your browser
+// 
+// To test that this worked
+// 1. Open Chrome to any webpage
+// 2. Open the dev console (cmd-option-i on Mac or ctrl-shift-i on Windows) and type: > navigator.serial
+// 3. If you see something like "Serial {onconnect:null, ondisconnect: null}" then it worked!
+//    If, instead, it says "undefined" then it didn't work. Try restarting your computer and then Chrome.
+//
+// TODOs:
+//  - Support binary in addition to text
+//
+// By Jon E. Froehlich
+// http://makeabilitylab.io/
+
+
+const SerialEvents = Object.freeze({
+    CONNECTION_OPENED: Symbol("Connection opened"),
+    CONNECTION_CLOSED: Symbol("Connection closed"),
+    DATA_RECEIVED: Symbol("New data received"),
+    ERROR_OCCURRED: Symbol("Error occurred"),
+  });
+  
+  class Serial {
+  
+    constructor() {
+      this.serialPort = null;
+  
+      this.textWriter = null;
+      this.dataReader = null;
+      this.keepReading = false;
+  
+      this.readableDataStreamClosed = null;
+      this.writableTextStreamClosed = null;
+  
+      // event handling https://stackoverflow.com/a/56612753
+      this.events = new Map();
+  
+      this.knownEvents = new Set(
+        [SerialEvents.CONNECTION_OPENED,
+        SerialEvents.CONNECTION_CLOSED,
+        SerialEvents.DATA_RECEIVED,
+        SerialEvents.ERROR_OCCURRED]);
+  
+      if (navigator.serial) {
+        navigator.serial.addEventListener("connect", (event) => {
+          console.log("navigator.serial event: connected!");
+        });
+  
+        navigator.serial.addEventListener("disconnect", (event) => {
+          console.log("navigator.serial event: disconnected!");
+          this.close();
+        });
+      }
+    }
+  
+    on(label, callback) {
+      if (this.knownEvents.has(label)) {
+        if (!this.events.has(label)) {
+          this.events.set(label, []);
+        }
+        this.events.get(label).push(callback);
+      } else {
+        console.log(`Could not create event subscription for ${label}. Event unknown.`);
+      }
+    }
+  
+    fireEvent(event, data = null) {
+      if (this.events.has(event)) {
+        for (let callback of this.events.get(event)) {
+          callback(this, data);
+        }
+      }
+    }
+  
+    /**
+     * Automatically connects and opens the previously approved port
+     * If there are more than one, it takes the top port in the approved port list
+     */
+    async autoConnectAndOpenPreviouslyApprovedPort(serialOptions = { baudRate: 9600 }) {
+      if (navigator.serial) {
+        const approvedPortList = await navigator.serial.getPorts();
+        console.log("approvedPortList", approvedPortList);
+  
+        if (approvedPortList.length > 0) {
+          console.log("Trying to auto-connect to:", approvedPortList[0].getInfo());
+          await this.connect(approvedPortList[0]);
+  
+          console.log("Attempting to open port:")
+          this.open(serialOptions);
+        }
+      }
+    }
+  
+    /**
+     * Returns true if open and active
+     */
+    isOpen() {
+      return this.serialPort && this.dataReader && this.textWriter;
+    }
+   
+    /**
+     * Writes out data as text
+     * @param {*} data 
+     */
+    async write(data) {
+      this.textWriter.write(data);
+    }
+  
+    /**
+     * Close and cleanup serial port
+     * Code based on https://web.dev/serial/#close-port
+     */
+    async close() {
+      //See https://reillyeon.github.io/serial/#close-method
+      if (this.dataReader) {
+        this.keepReading = false;
+        this.dataReader.cancel();
+  
+        await this.readableDataStreamClosed.catch(() => { /* Ignore the error */ });
+        this.dataReader = null;
+        this.readableDataStreamClosed = null;
+      }
+  
+      if (this.textWriter) {
+        await this.textWriter.close();
+        await this.writableTextStreamClosed;
+  
+        this.textWriter = null;
+        this.writableTextStreamClosed = null;
+      }
+  
+      if (this.serialPort) {
+        await this.serialPort.close();
+        this.serialPort = null;
+      }
+  
+      this.fireEvent(SerialEvents.CONNECTION_CLOSED);
+    }
+  
+    /**
+     * Prompts user for approval to connect to a serial device and opens the port to
+     * approved device 
+     * 
+     * @param {dictionary} portFilters 
+     * @param {dictionary} serialOptions 
+     */
+    async connectAndOpen(portFilters = null, serialOptions = { baudRate: 9600 }) {
+      await this.connect(null, portFilters);
+  
+      if (this.serialPort){
+        this.open(serialOptions);
+      }
+    }
+  
+    /**
+     * Attempts to connect to the existing port (if provided). Otherwise prompts the user
+     * to connect to a new serial device (with the portFilters, if provided)
+     * 
+     * @param {port} existingPort 
+     * @param {dictionary} portFilters https://reillyeon.github.io/serial/#serialportfilter-dictionary
+     */
+    async connect(existingPort = null, portFilters = null) {
+      try {
+        // Prompt user to select any serial port.
+        // The navigator.serial.requestPort() function takes an optional object literal that defines filters. 
+        // Those are used to match any serial device connected over USB with a mandatory USB vendor (usbVendorId) 
+        // and optional USB product identifiers (usbProductId). So, for example:
+        //
+        //   // start example code
+        //   // Filter on devices with the Arduino Uno USB Vendor/Product IDs.
+        //   const filters = [
+        //     { usbVendorId: 0x2341, usbProductId: 0x0043 },
+        //     { usbVendorId: 0x2341, usbProductId: 0x0001 }
+        //   ];
+        //
+        //   // Prompt user to select an Arduino Uno device.
+        //   const port = await navigator.serial.requestPort({ filters });
+        //
+        //   const { usbProductId, usbVendorId } = port.getInfo();
+        //   // end example code
+        //
+  
+        // Get all serial ports the user has previously granted the website access to.
+        const oldApprovedPortList = await navigator.serial.getPorts();
+  
+        if (!existingPort) {
+          // if the user does not pass in an existing port 
+          this.serialPort = await navigator.serial.requestPort(portFilters);
+        } else if (!oldApprovedPortList.includes(existingPort)) {
+          // if the passed in port is not actually in the approved list
+          console.log("The port", existingPort.getInfo(), " was not previously approved, prompting user");
+          this.serialPort = await navigator.serial.requestPort(portFilters);
+        } else {
+          console.log("Attempting connection to pre-approved port: ", existingPort.getInfo());
+          this.serialPort = existingPort;
+        }
+  
+        const newApprovedPortList = await navigator.serial.getPorts();
+  
+        console.log("Approved ports:");
+        newApprovedPortList.forEach((port, index) => console.log(index, ":", port.getInfo()));
+  
+        console.log("Selected port:");
+        console.log(this.serialPort.getInfo());
+        console.log(this.serialPort);
+      } catch (error) {
+        this.fireEvent(SerialEvents.ERROR_OCCURRED, error);
+      }
+    }
+  
+    /**
+     * Connects to the Web Serial port and starts listening to serial input.
+     * Defaults to a baud rate of 9600
+     * 
+     * @param {dictionary} serialOptions See https://reillyeon.github.io/serial/#dom-serialoptions
+     */
+    async open(serialOptions = { baudRate: 9600 }) {
+        try {
+          // Open the serial port
+        // This function takes in a SerialOptions dictionary where baudRate is the only required member
+        // https://reillyeon.github.io/serial/#dom-serialoptions
+        await this.serialPort.open(serialOptions);
+        console.log("Opened serial port with settings:", serialOptions);
+
+        // Setup serial output stream as text
+        const textEncoder = new TextEncoderStream();
+        this.writableTextStreamClosed = textEncoder.readable.pipeTo(this.serialPort.writable);
+        this.textWriter = textEncoder.writable.getWriter();
+    
+        // Setup serial input stream as text
+        this.keepReading = true;
+        this.readableDataStreamClosed = this.serialPort.readable;
+        this.dataReader = this.serialPort.readable.getReader();
+
+        this.fireEvent(SerialEvents.CONNECTION_OPENED);
+
+        this.readLoop();
+        
+      } catch (error) {
+        // handle non-fatal error
+        this.fireEvent(SerialEvents.ERROR_OCCURRED, error);
+      }
+    }
+
+    async readLoop()
+    {
+      while (this.serialPort.readable && this.keepReading) {
+        try {
+          while (true) {
+            const { value, done } = await this.dataReader.read();
+
+            if (done) {
+              this.dataReader.releaseLock();
+              break;
+            }
+
+            if (value) {
+              this.fireEvent(SerialEvents.DATA_RECEIVED, value);
+            }
+          }
+
+        } catch (error) {
+          // handle non-fatal error
+          this.fireEvent(SerialEvents.ERROR_OCCURRED, error);
+        }
+        finally {
+          // see https://reillyeon.github.io/serial/#close-method
+          this.dataReader.releaseLock();
+        }
+      }
+    }
+  }
+  
